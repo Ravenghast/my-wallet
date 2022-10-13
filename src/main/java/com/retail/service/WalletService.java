bf0370303c9f8e795015112c7a4e12482f621792
Ravenghast <raj.nkarthik@gmail.com>
2022-10-14 01:39:32 +0530

    handled more conditions

--- src/main/java/com/retail/service/WalletService.java
+++ src/main/java/com/retail/service/WalletService.java
@@ -1,5 +1,6 @@
 package com.retail.service;
 
+import com.google.common.collect.Sets;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,8 @@ import java.util.*;
 public class WalletService implements IWalletService {
 
     private final @Getter HashMap<Integer, Integer> wallet = new HashMap<>();
-    private final @Getter List<Integer> denominations  = new ArrayList<>();
+    private final List<Integer> txn = new ArrayList<>();
+    private int amount;
 
     @Override
     public void initialiseWallet(List<Integer> list) {
@@ -20,7 +22,6 @@ public class WalletService implements IWalletService {
             throw new IllegalArgumentException("Invalid wallet");
         }
         wallet.clear();
-        denominations.clear();
         for (int note : list) {
             if (note <= 0) {
                 log.error("invalid denomination "+note);
@@ -28,20 +29,19 @@ public class WalletService implements IWalletService {
             }
             wallet.merge(note, 1, Integer::sum);
         }
-        denominations.addAll(wallet.keySet());
-        denominations.sort(Collections.reverseOrder());
     }
 
     @Override
-    public List<Integer> getNotes(int amount) {
-        if (amount <= 0) {
+    public List<Integer> getNotes(int txnAmount) {
+        if (txnAmount <= 0) {
             log.error("invalid amount "+ amount);
             throw new IllegalArgumentException("Invalid amount: "+amount);
         }
-        List<Integer> txn = new ArrayList<>();
-        for (int note : denominations) {
-            if (amount >= note) {
-                amount = extractAmount(amount, note, txn);
+        for (int i = 1; i <= wallet.keySet().size(); i++) {
+            List<List<Integer>> comboList = generateCombinations(i);
+            verifyCombination(comboList, txnAmount);
+            if (amount == 0) {
+                break;
             }
         }
         if (amount > 0) {
@@ -52,16 +52,49 @@ public class WalletService implements IWalletService {
         return txn;
     }
 
-    private int extractAmount(int value, int note, List<Integer> txn) {
-        int count = wallet.get(note);
-        while (count-- > 0) {
-            txn.add(note);
-            value -= note;
-            if (note > value) {
+    private List<List<Integer>> generateCombinations(int n) {
+        Set<Set<Integer>> combinations = Sets.combinations(wallet.keySet(), n);
+        List<List<Integer>> comboList = new ArrayList<>();
+        combinations.forEach(set -> {
+            List<Integer> list = new ArrayList<>(set);
+            list.sort(Collections.reverseOrder());
+            comboList.add(list);
+        });
+        comboList.sort((x, y) -> {
+            for (int i = 0; i < Math.max(x.size(), y.size()); i++) {
+                if (!Objects.equals(x.get(i), y.get(i))) {
+                    return y.get(i) - x.get(i);
+                }
+            }
+            return y.size() - x.size();
+        });
+        return comboList;
+    }
+
+    private void verifyCombination(List<List<Integer>> comboList, int txnAmount) {
+        for (List<Integer> nextCombo : comboList) {
+            amount = txnAmount;
+            txn.clear();
+            for (int note : nextCombo) {
+                if (amount >= note) {
+                    extractAmount(note);
+                }
+            }
+            if (amount == 0) {
                 break;
             }
         }
-        return value;
+    }
+
+    private void extractAmount(int note) {
+        int count = wallet.get(note);
+        while (count-- > 0) {
+            txn.add(note);
+            amount -= note;
+            if (note > amount) {
+                break;
+            }
+        }
     }
 
 }
--- src/test/java/com/retail/service/WalletServiceTest.java
+++ src/test/java/com/retail/service/WalletServiceTest.java
@@ -14,7 +14,6 @@ public class WalletServiceTest {
     @Test
     public void testInitialiseWallet() {
         int[] test = {20, 1, 10, 1, 50, 5, 50, 1, 20};
-        int[] expected = {50, 20, 10, 5, 1};
         HashMap<Integer, Integer> expectedMap = new HashMap<>();
         expectedMap.put(20, 2);
         expectedMap.put(1, 3);
@@ -22,13 +21,10 @@ public class WalletServiceTest {
         expectedMap.put(50, 2);
         expectedMap.put(5, 1);
         List<Integer> testList = convertToList(test);
-        List<Integer> expectedList = convertToList(expected);
         Set<Map.Entry<Integer, Integer>> expectedEntries = expectedMap.entrySet();
         mgr.initialiseWallet(testList);
-        List<Integer> denominations = mgr.getDenominations();
         HashMap<Integer, Integer> wallet = mgr.getWallet();
         Set<Map.Entry<Integer, Integer>> walletSet = wallet.entrySet();
-        assertIterableEquals(expectedList, denominations);
         assertIterableEquals(expectedEntries, walletSet);
     }
 
@@ -56,6 +52,50 @@ public class WalletServiceTest {
         List<Integer> expectedList = convertToList(expected);
         mgr.initialiseWallet(testList);
         List<Integer> notes = mgr.getNotes(7);
+        assertIterableEquals(expectedList, notes);
+    }
+
+    @Test
+    public void testGetNotesSuccess1() {
+        int[] test = {2, 2, 2, 5, 10};
+        int[] expected = {5, 2, 2, 2};
+        List<Integer> testList = convertToList(test);
+        List<Integer> expectedList = convertToList(expected);
+        mgr.initialiseWallet(testList);
+        List<Integer> notes = mgr.getNotes(11);
+        assertIterableEquals(expectedList, notes);
+    }
+
+    @Test
+    public void testGetNotesSuccess2() {
+        int[] test = {2, 1, 1, 2, 7, 10};
+        int[] expected = {10, 2, 2, 1, 1};
+        List<Integer> testList = convertToList(test);
+        List<Integer> expectedList = convertToList(expected);
+        mgr.initialiseWallet(testList);
+        List<Integer> notes = mgr.getNotes(16);
+        assertIterableEquals(expectedList, notes);
+    }
+
+    @Test
+    public void testGetNotesSuccess3() {
+        int[] test = {2, 2, 2, 5, 7, 10};
+        int[] expected = {10, 2, 2, 2};
+        List<Integer> testList = convertToList(test);
+        List<Integer> expectedList = convertToList(expected);
+        mgr.initialiseWallet(testList);
+        List<Integer> notes = mgr.getNotes(16);
+        assertIterableEquals(expectedList, notes);
+    }
+
+    @Test
+    public void testGetNotesSuccess4() {
+        int[] test = {1, 50, 5, 10, 1, 20, 20, 5, 1, 50};
+        int[] expected = {50, 50, 20, 1, 1, 1};
+        List<Integer> testList = convertToList(test);
+        List<Integer> expectedList = convertToList(expected);
+        mgr.initialiseWallet(testList);
+        List<Integer> notes = mgr.getNotes(123);
         assertIterableEquals(expectedList, notes);
     }
 
